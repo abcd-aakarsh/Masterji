b0d5fbdfea97df8edce3aab7a307885eea7fe030
Aakarsh Verma <ansh62050@gmail.com>
2025-03-23 23:44:46 +0530

    [Cohort] JS Assignment 4 - Base UI Completed

---
+++ [Cohort] JS Assignment 4/app.js
@@ -0,0 +1,53 @@
+const book = {
+  kind: "books#volume",
+  id: 1,
+  etag: "KfkbeLGcmEA",
+  volumeInfo: {
+    title: "Practices of an Agile Developer",
+    subtitle: "Working in the Real World",
+    authors: ["Venkat Subramaniam", "Andy Hunt"],
+    publisher: "Pragmatic Bookshelf",
+    publishedDate: "2006-04-04",
+    description:
+      "These are the proven, effective agile practices that will make you a better developer. You'll learn pragmatic ways of approaching the development process and your personal coding techniques. You'll learn about your own attitudes, issues with working on a team, and how to best manage your learning, all in an iterative, incremental, agile style. You'll see how to apply each practice, and what benefits you can expect. Bottom line: This book will make you a better developer.",
+    industryIdentifiers: [
+      {
+        type: "ISBN_13",
+        identifier: "9781680504149",
+      },
+      {
+        type: "ISBN_10",
+        identifier: "1680504142",
+      },
+    ],
+    readingModes: {
+      text: true,
+      image: true,
+    },
+    pageCount: 237,
+    printType: "BOOK",
+    categories: ["Computers"],
+    averageRating: 3,
+    ratingsCount: 4,
+    maturityRating: "NOT_MATURE",
+    allowAnonLogging: true,
+    contentVersion: "1.2.3.0.preview.3",
+    panelizationSummary: {
+      containsEpubBubbles: false,
+      containsImageBubbles: false,
+    },
+    imageLinks: {
+      smallThumbnail:
+        "http://books.google.com/books/content?id=KA9QDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+      thumbnail:
+        "http://books.google.com/books/content?id=KA9QDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    },
+    language: "en",
+    previewLink:
+      "http://books.google.co.in/books?id=KA9QDwAAQBAJ&printsec=frontcover&dq=developer&hl=&cd=1&source=gbs_api",
+    infoLink:
+      "https://play.google.com/store/books/details?id=KA9QDwAAQBAJ&source=gbs_api",
+    canonicalVolumeLink:
+      "https://play.google.com/store/books/details?id=KA9QDwAAQBAJ",
+  },
+};
