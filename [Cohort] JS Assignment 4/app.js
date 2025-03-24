fba5a93b353a4bef44003841db24c5a3db1f1055
Aakarsh Verma <ansh62050@gmail.com>
2025-03-25 00:09:50 +0530

    [Cohort] JS Assignment 4 - Searching and different views

--- [Cohort] JS Assignment 4/app.js
+++ [Cohort] JS Assignment 4/app.js
@@ -1,53 +1,132 @@
-const book = {
-  kind: "books#volume",
-  id: 1,
-  etag: "KfkbeLGcmEA",
-  volumeInfo: {
-    title: "Practices of an Agile Developer",
-    subtitle: "Working in the Real World",
-    authors: ["Venkat Subramaniam", "Andy Hunt"],
-    publisher: "Pragmatic Bookshelf",
-    publishedDate: "2006-04-04",
-    description:
-      "These are the proven, effective agile practices that will make you a better developer. You'll learn pragmatic ways of approaching the development process and your personal coding techniques. You'll learn about your own attitudes, issues with working on a team, and how to best manage your learning, all in an iterative, incremental, agile style. You'll see how to apply each practice, and what benefits you can expect. Bottom line: This book will make you a better developer.",
-    industryIdentifiers: [
-      {
-        type: "ISBN_13",
-        identifier: "9781680504149",
-      },
-      {
-        type: "ISBN_10",
-        identifier: "1680504142",
-      },
-    ],
-    readingModes: {
-      text: true,
-      image: true,
-    },
-    pageCount: 237,
-    printType: "BOOK",
-    categories: ["Computers"],
-    averageRating: 3,
-    ratingsCount: 4,
-    maturityRating: "NOT_MATURE",
-    allowAnonLogging: true,
-    contentVersion: "1.2.3.0.preview.3",
-    panelizationSummary: {
-      containsEpubBubbles: false,
-      containsImageBubbles: false,
-    },
-    imageLinks: {
-      smallThumbnail:
-        "http://books.google.com/books/content?id=KA9QDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-      thumbnail:
-        "http://books.google.com/books/content?id=KA9QDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-    },
-    language: "en",
-    previewLink:
-      "http://books.google.co.in/books?id=KA9QDwAAQBAJ&printsec=frontcover&dq=developer&hl=&cd=1&source=gbs_api",
-    infoLink:
-      "https://play.google.com/store/books/details?id=KA9QDwAAQBAJ&source=gbs_api",
-    canonicalVolumeLink:
-      "https://play.google.com/store/books/details?id=KA9QDwAAQBAJ",
-  },
+const url =
+  "https://api.freeapi.app/api/v1/public/books?page=1&limit=10&inc=kind%252Cid%252Cetag%252CvolumeInfo";
+
+const fetchBooks = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data.data.data);
+  const booksData = data.data.data;
+  localStorage.setItem("booksData", JSON.stringify([...booksData]));
+  console.log(booksData);
+  displayBooks(booksData);
 };
+fetchBooks();
+
+const randomQuote = async () => {
+  const response = await fetch(
+    "https://api.freeapi.app/api/v1/public/books/book/random"
+  );
+  const data = await response.json();
+  const book = data.data;
+  const bookTitle = document.getElementById("book-title");
+  bookTitle.innerText = book.volumeInfo.title;
+  const bookDesc = document.getElementById("book-desc");
+  bookDesc.innerText = book.volumeInfo.description;
+  const authorName = document.getElementById("author-name");
+  authorName.innerText = `- ${book.volumeInfo.authors[0]}`;
+  const heroBtn = document.getElementById("hero-btn");
+  heroBtn.setAttribute("href", book.volumeInfo?.infoLink);
+  const heroImg = document.getElementById("hero-img");
+  const imageUrl = book.volumeInfo?.imageLinks?.thumbnail;
+  heroImg.setAttribute("src", imageUrl);
+};
+randomQuote();
+
+const displayBooks = (booksData) => {
+  const booksContainer = document.querySelector(".books-container");
+  booksData.forEach((book) => {
+    const bookContainer = document.createElement("div");
+    const imgDiv = document.createElement("div");
+    const img = document.createElement("img");
+    const bookDesc = document.createElement("div");
+    const bookTitle = document.createElement("h2");
+    const author = document.createElement("p");
+    const bookPara = document.createElement("p");
+    const rating = document.createElement("p");
+    const btn = document.createElement("a");
+
+    img.setAttribute("src", book.volumeInfo.imageLinks.thumbnail);
+    img.setAttribute("alt", "book");
+    imgDiv.appendChild(img);
+    bookTitle.innerText = book.volumeInfo.title;
+    author.innerText = book.volumeInfo.authors[0];
+    bookPara.innerText = book.volumeInfo.description;
+    bookPara.classList.add("text");
+    rating.innerText = `AvgRating: ${
+      book.volumeInfo?.averageRating
+        ? book.volumeInfo?.averageRating
+        : "Not Rated"
+    }`;
+    btn.innerHTML = "Know More";
+    btn.setAttribute("href", book.volumeInfo.infoLink);
+    btn.setAttribute("target", "_blank");
+    btn.classList.add("btn");
+    bookDesc.classList.add("book-desc-list");
+    bookDesc.classList.add("book-desc");
+    bookContainer.classList.add("book");
+    bookContainer.classList.add("book-list-view");
+
+    bookDesc.appendChild(bookTitle);
+    bookDesc.appendChild(author);
+    bookDesc.appendChild(bookPara);
+    bookDesc.appendChild(rating);
+    bookDesc.appendChild(btn);
+    bookContainer.appendChild(imgDiv);
+    bookContainer.appendChild(bookDesc);
+    booksContainer.appendChild(bookContainer);
+  });
+};
+
+const gridView = document.getElementById("grid");
+gridView.addEventListener("click", () => {
+  const booksContainer = document.querySelector(".books-container");
+  const bookContainer = document.querySelectorAll(".book");
+  bookContainer.forEach((e) => {
+    e.classList.remove("book-list-view");
+    e.classList.add("grid-book-view");
+  });
+
+  booksContainer.classList.add("grid-view");
+  booksContainer.classList.remove("list-view");
+});
+
+const listView = document.getElementById("list");
+listView.addEventListener("click", () => {
+  const booksContainer = document.querySelector(".books-container");
+  const bookContainer = document.querySelectorAll(".book");
+
+  bookContainer.forEach((e) => {
+    e.classList.add("book-list-view");
+    e.classList.remove("grid-book-view");
+  });
+
+  booksContainer.classList.remove("grid-view");
+  booksContainer.classList.add("list-view");
+});
+
+const inputBox = document.querySelector(".input-box");
+
+const filterList = () => {
+  const searchingValue = inputBox.value.trim();
+  let books = JSON.parse(localStorage.getItem("booksData"));
+
+  const filteredBooks = books.filter((book) => {
+    return (
+      book.volumeInfo?.title
+        ?.toLowerCase()
+        .includes(searchingValue.toLowerCase()) ||
+      book.volumeInfo?.authors[0]
+        ?.toLowerCase()
+        .includes(searchingValue.toLowerCase())
+    );
+  });
+  const booksContainer = document.querySelector(".books-container");
+  booksContainer.innerHTML = "";
+  displayBooks(filteredBooks);
+};
+
+inputBox.addEventListener("input", filterList);
+
+const sort = document.getElementById("sort");
+
+// sort.addEventListener("change", listener);
